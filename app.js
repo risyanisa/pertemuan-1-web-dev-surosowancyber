2ae82a738f7e7b8880ae7426d27493737c81f778
risyanisa <risyanisa74@gmail.com>
2026-06-13 17:01:26 +0700

    progres ke 3

---
+++ app.js
@@ -0,0 +1,27 @@
+//operator aritmatika
+let a = 10
+let b = 5
+
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+console.log(a % b)
+console.log(a ** b)
+
+//operator perbandingan
+let nilai = 80;
+
+console.log(nilai > 70) //true
+console.log(nilai < 70) //false
+console.log(nilai == "81")//true
+console.log(nilai !== 90)//true
+
+
+//operator logika
+let usernameBenar = true;
+let passwordBenar = true;
+
+console.log(usernameBenar && passwordBenar); //true
+console.log(usernameBenar || passwordBenar);//true
+console.log(!usernameBenar); //false
